341ba4c140779567851b4ad1c9167de5db321e69
Kasun <kasun.kanakarathne@unicast.ne.jp>
2026-09-22 19:22:15 +0900

    docs: React + Webpack + Babel + react-router + CSS Modulesのセットアップ

---
+++ src/routes.jsx
@@ -0,0 +1,8 @@
+const routes = [
+  {
+    path: "/",
+    element: <div>Hello, Shopping Cart!</div>,
+  },
+];
+
+export default routes;
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+import path from "node:path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+export default {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(import.meta.dirname, "dist"),
+    clean: true,
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.module\.css$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        exclude: /\.module\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./template.html",
+    }),
+  ],
+  devServer: {
+    port: 8080,
+    historyApiFallback: true, // needed so client-side routes work on refresh
+    watchFiles: ["./template.html"],
+  },
+};
