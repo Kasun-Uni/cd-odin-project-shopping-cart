b371ed0c8eae57f3774a95e887e04341bb40ed2e
Kasun <kasun.kanakarathne@unicast.ne.jp>
2026-09-22 19:49:54 +0900

    feat: Navbarにリアルタイムのカート商品数バッジを追加

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,13 @@
 import { Link } from "react-router";
+import { useCart } from "../context/CartContext.jsx";
+import styles from "./Navbar.module.css";
 
 function Navbar() {
+  const { totalItems } = useCart();
+
   return (
-    <nav>
-      <ul>
+    <nav className={styles.navbar}>
+      <ul className={styles.navList}>
         <li>
           <Link to="/">Home</Link>
         </li>
@@ -11,7 +15,12 @@ function Navbar() {
           <Link to="/shop">Shop</Link>
         </li>
         <li>
-          <Link to="/cart">Cart</Link>
+          <Link to="/cart" className={styles.cartLink}>
+            Cart
+            {totalItems > 0 && (
+              <span className={styles.badge}>{totalItems}</span>
+            )}
+          </Link>
         </li>
       </ul>
     </nav>
